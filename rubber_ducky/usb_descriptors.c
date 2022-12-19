270ece037cc8cf7dfb4dc633a41e8547cca71e1a
Hung Do <hungdojan@gmail.com>
2022-12-19 14:18:13 +0100

    Finalized USB descriptor

--- rubber_ducky/usb_descriptors.c
+++ rubber_ducky/usb_descriptors.c
@@ -19,11 +19,7 @@
 #include "class/hid/hid_device.h"   // CFG_TUD_HID_EP_BUFSIZE
 #include "usb_reports.h"
 
-// These makros are taken from TinyUSB example file
-// https://github.com/hathach/tinyusb/blob/4bfab30c02279a0530e1a56f4a7c539f2d35a293/examples/device/hid_composite/src/usb_descriptors.c
-#define _PID_MAP(itf, n)  ( (CFG_TUD_##itf) << (n) )
-#define USB_PID           (0x4000 | _PID_MAP(CDC, 0) | _PID_MAP(MSC, 1) | _PID_MAP(HID, 2) | \
-                          _PID_MAP(MIDI, 3) | _PID_MAP(VENDOR, 4) )
+#define _CONFIG_TOTAL_LENGTH (9+9+9+7)
 
 // device descriptor
 tusb_desc_device_t const desc_device = {
@@ -47,30 +43,23 @@ tusb_desc_device_t const desc_device = {
     .bNumConfigurations = 0x01      // number of configuration the device supports
 };
 
-uint8_t const * tud_descriptor_device_cb() {
-    return (uint8_t const *) &desc_device;
-}
-
-#define _CONFIG_TOTAL_LENGTH sizeof(tusb_desc_interface_t) \
-                             + sizeof(tusb_hid_descriptor_hid_t) \
-                             + sizeof(tusb_desc_endpoint_t)
 tusb_desc_configuration_t const desc_config = {
-    .bLength             = sizeof(tusb_desc_configuration_t),
+    .bLength             = 9, // can't use sizeof due to padding
     .bDescriptorType     = TUSB_DESC_CONFIGURATION,
     .wTotalLength        = _CONFIG_TOTAL_LENGTH,
     .bNumInterfaces      = 0x01,
     .bConfigurationValue = 0x01,
     .iConfiguration      = 0x00,
-    .bmAttributes        = TU_BIT(7) | 0x00,
+    .bmAttributes        = TU_BIT(7) | TU_BIT(5) | 0x00,    // self-powered
     .bMaxPower           = 50
 };
 
 tusb_desc_interface_t const desc_interface = {
-    .bLength            = sizeof(tusb_desc_interface_t),
+    .bLength            = 9, // can't use sizeof due to padding
     .bDescriptorType    = TUSB_DESC_INTERFACE,
     .bInterfaceNumber   = 0x00,
     .bAlternateSetting  = 0x00,
-    .bNumEndpoints      = 0x00,
+    .bNumEndpoints      = 0x01,
     .bInterfaceClass    = TUSB_CLASS_HID,
     .bInterfaceSubClass = 0x00,
     .bInterfaceProtocol = 0x00,
@@ -78,7 +67,7 @@ tusb_desc_interface_t const desc_interface = {
 };
 
 tusb_hid_descriptor_hid_t const desc_hid = {
-    .bLength            = sizeof(tusb_hid_descriptor_hid_t),
+    .bLength            = 9, // can't use sizeof due to padding
     .bDescriptorType    = HID_DESC_TYPE_HID,
     .bcdHID             = 0x0100,
     .bCountryCode       = 0x00,
@@ -88,32 +77,15 @@ tusb_hid_descriptor_hid_t const desc_hid = {
 };
 
 tusb_desc_endpoint_t const desc_endpoint = {
-    .bLength            = sizeof(tusb_desc_endpoint_t),
+    .bLength            = 7, // can't use sizeof due to padding
     .bDescriptorType    = TUSB_DESC_ENDPOINT,
     .bEndpointAddress   = 0x81,
-    .bmAttributes       = {.xfer=TUSB_XFER_INTERRUPT, 0, },
+    .bmAttributes       = {.xfer=TUSB_XFER_INTERRUPT},
     .wMaxPacketSize     = {.size=CFG_TUD_HID_EP_BUFSIZE},
-    .bInterval          = 10,
+    .bInterval          = 5,
 };
 
-#define _CONFIG_BUFFER_SIZE 9+9+9+7
-static uint8_t config_buffer[_CONFIG_BUFFER_SIZE] = {
-    9, TUSB_DESC_CONFIGURATION, _CONFIG_TOTAL_LENGTH, 1, 1, 0, TU_BIT(7)|0, 50
-};
-
-uint8_t const * tud_descriptor_configuration_cb(uint8_t index) {
-    (void) index;
-    // TODO: doesn't work for some reason
-    size_t i = 0;
-    memcpy(config_buffer+i, (uint8_t *)&desc_config, desc_config.bLength);
-    i += desc_config.bLength;
-    memcpy(config_buffer+i, &desc_interface, desc_interface.bLength);
-    i += desc_interface.bLength;
-    memcpy(config_buffer+i, &desc_hid, desc_hid.bLength);
-    i += desc_hid.bLength;
-    memcpy(config_buffer+i, &desc_endpoint, desc_endpoint.bLength);
-    return (uint8_t const *) &config_buffer;
-}
+uint8_t config_buffer[_CONFIG_TOTAL_LENGTH] = { 0, };
 
 static char *string_descriptor[] = {
     "BUT FIT",          // Manufacture
@@ -122,19 +94,52 @@ static char *string_descriptor[] = {
 
 static uint16_t _desc_buffer[32] = { 0, };
 
+uint8_t const * tud_descriptor_device_cb() {
+    return (uint8_t const *) &desc_device;
+}
+
+uint8_t const * tud_descriptor_configuration_cb(uint8_t index) {
+    (void) index;
+    // combine config, interface, hid and endpoint descriptors together
+    size_t offset = 0;
+    // Configuration descriptor
+    memcpy(config_buffer + offset, (uint8_t *)&desc_config, desc_config.bLength);
+    offset += desc_config.bLength;
+
+    // Interface descriptor
+    memcpy(config_buffer + offset, (uint8_t *)&desc_interface, desc_interface.bLength);
+    offset += desc_interface.bLength;
+
+    // HID descriptor
+    memcpy(config_buffer + offset, (uint8_t *)&desc_hid, desc_hid.bLength);
+    offset += desc_hid.bLength;
+
+    // Endpoint descriptor
+    memcpy(config_buffer + offset, (uint8_t *)&desc_endpoint, desc_endpoint.bLength);
+
+    return (uint8_t const *) config_buffer;
+}
+
 uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
     (void) langid;
     uint8_t bLength;
+
+    // to identify language (first index)
     if (index == 0) {
         _desc_buffer[1] = 0x0409;
         bLength = 1;
     } else {
+        // other string descriptions
+        // ignore indices that are greater than
+        // the size of string array "string_descriptor"
         if (index > 2)
             return NULL;
+
         bLength = strlen(string_descriptor[index-1]);
         // trim access characters
-        if (bLength > 32)
+        if (bLength > 32) {
             bLength = 31;
+        }
 
         // convert ASCII to UTF-16
         for (uint8_t i = 0; i < bLength; i++) {
