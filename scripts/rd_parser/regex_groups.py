f95e762e84e34465ea7a25fdabc7dc50a2698147
Hung Do <hungdojan@gmail.com>
2023-03-09 23:29:15 +0100

    Updated parser structure

--- scripts/rd_parser/regex_groups.py
+++ scripts/rd_parser/regex_groups.py
@@ -4,10 +4,10 @@ class Groups(enum.Enum):
     DELAY_WAIT_ORIGINAL = 0
     DELAY_WAIT_VALUE    = 1
     SPECIAL_ORIGINAL    = 2
-    SPECIAL_CONTENT     = 3
+    SPECIAL_WHOLE       = 3 # probably for debugging purposes
     SPECIAL_MODIFIERS   = 4
     SPECIAL_ESCAPE_EN   = 5
-    SPECIAL_KEYS_OR_ID  = 6
+    SPECIAL_VALUE       = 6 # ids are keys like escape or enter
     COMMENT             = 7
     NORMAL_KEYS         = 8
 
